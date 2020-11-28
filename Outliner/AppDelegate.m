5878f7aabb6fe20208fb6f19f2fb8a0706272247
Yohanes Bandung Bondowoso <hi@ybbond.dev>
2020-11-29 01:18:56 +0700

    successfully show a window without storyboard

--- Outliner/AppDelegate.m
+++ Outliner/AppDelegate.m
@@ -9,6 +9,7 @@
 
 @interface AppDelegate ()
 
+@property (nonatomic, strong) NSWindow *activeWindow;
 
 @end
 
@@ -16,6 +17,24 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    // A little help from this site: https://medium.com/@MalikAlayli/create-a-macos-app-without-a-main-storyboard-81f4ee8be1d
+    // Eventhough that uses Swift
+    
+    NSSize windowSize = NSMakeSize(480, 480);
+    NSRect screenSize = NSScreen.mainScreen.frame;
+    NSRect rect = NSMakeRect(screenSize.size.width/2 - windowSize.width/2, screenSize.size.height/2 - windowSize.height/2, windowSize.width, windowSize.height);
+    NSWindowStyleMask styleMask = NSWindowStyleMaskResizable|NSWindowStyleMaskClosable|NSWindowStyleMaskTitled|NSWindowStyleMaskMiniaturizable;
+    NSBackingStoreType backing = NSBackingStoreBuffered;
+
+    
+    NSWindow *activeWindow = [[NSWindow alloc] initWithContentRect:rect
+                             styleMask:styleMask
+                               backing:backing
+                                 defer:NO];
+    
+    activeWindow.title = @"Outliner";
+    [activeWindow makeKeyAndOrderFront:NSApp];
 }
 
 
--- Outliner/main.m
+++ Outliner/main.m
@@ -6,10 +6,26 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-    }
-    return NSApplicationMain(argc, argv);
+//    @autoreleasepool {
+//        // Setup code that might create autoreleased objects goes here.
+//    }
+//    return NSApplicationMain(argc, argv);
+//
+//    removing above code and use the below code because of the answer from this
+//    StackOverflow: https://stackoverflow.com/questions/15694510/programmatically-create-initial-window-of-cocoa-app-os-x
+    
+    
+    
+    NSArray *tl;
+    NSApplication *application = [NSApplication sharedApplication];
+    [[NSBundle mainBundle] loadNibNamed:@"MainMenu" owner:application topLevelObjects:&tl];
+
+    AppDelegate *applicationDelegate = [[AppDelegate alloc] init];      // Instantiate App  delegate
+    [application setDelegate:applicationDelegate];                      // Assign delegate to the NSApplication
+    [application run];                                                  // Call the Apps Run method
+
+    return 0;       // App Never gets here.
 }
